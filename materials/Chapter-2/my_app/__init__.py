ade2607f593880d36a6f19a4f041f0db251fb533
spideynolove <spideynolove@gmail.com>
2024-04-06 17:20:56 +0700

    Add chapter 2 source code

--- materials/Chapter-2/my_app/__init__.py
+++ materials/Chapter-2/my_app/__init__.py
@@ -9,15 +9,30 @@ app.register_blueprint(product_blueprint)
 
 @app.template_filter('format_currency')
 def format_currency_filter(amount):
-	currency_code = ccy.countryccy(request.accept_languages.best[-2:]) or 'USD'
-	return '{0} {1}'.format(currency_code, amount)
+    currency_code = ccy.countryccy(request.accept_languages.best[-2:]) or 'USD'
+    return '{0} {1}'.format(currency_code, amount)
+
+
+@app.template_filter('product_color')
+def product_color_filter(product_name):
+    product_colors = {
+        'iPhone': 'orange-text',
+        'Samsung': 'purple-text',
+        'iPad': 'green-text',
+        'Google': 'red-text',
+        'Sony': 'blue-text',
+        'LG': 'pink-text',
+        'Nokia': 'cyan-text',
+        'Xiaomi': 'yellow-text',
+    }
+    brand = product_name.split(' ', 1)[0]
+    return product_colors.get(brand, 'black-text')
 
 
 class momentjs:
     def __init__(self, timestamp):
         self.timestamp = timestamp
 
-    # Wrapper to call moment.js method
     def render(self, format):
         return Markup(
             "<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (
@@ -25,7 +40,6 @@ class momentjs:
             )
         )
 
-    # Format time
     def format(self, fmt):
         return self.render("format(\"%s\")" % fmt)
 
--- materials/Chapter-2/my_app/product/models.py
+++ materials/Chapter-2/my_app/product/models.py
@@ -1,27 +1,98 @@
-# Updated PRODUCTS with 'nickname' field
 PRODUCTS = {
-    'Iphone': {
+    'iphone_5s': {
         'name': 'iPhone 5S',
         'category': 'Phones',
         'price': 699,
-        'nickname': 'Apple5S-12345'  # Example IMEI or nickname
+        'nickname': 'Apple5S-12345'
     },
-    'galaxy': {
-        'name': 'Samsung Galaxy 5',
+    'samsung_galaxy_s5': {
+        'name': 'Samsung Galaxy S5',
         'category': 'Phones',
         'price': 649,
-        'nickname': 'Galaxy5-67890'  # Example IMEI or nickname
+        'nickname': 'GalaxyS5-67890'
     },
-    'ipad-air': {
+    'ipad_air': {
         'name': 'iPad Air',
         'category': 'Tablets',
         'price': 649,
-        'nickname': 'iPadAir-54321'  # Example IMEI or nickname
+        'nickname': 'iPadAir-54321'
     },
-    'ipad-mini': {
+    'ipad_mini': {
         'name': 'iPad Mini',
         'category': 'Tablets',
         'price': 549,
-        'nickname': 'Mini-09876'  # Example IMEI or nickname
-    }
+        'nickname': 'Mini-09876'
+    },
+    'google_pixel_5': {
+        'name': 'Google Pixel 5',
+        'category': 'Phones',
+        'price': 699,
+        'nickname': 'Pixel5-98765'
+    },
+    'oneplus_9_pro': {
+        'name': 'OnePlus 9 Pro',
+        'category': 'Phones',
+        'price': 899,
+        'nickname': 'OnePlus9Pro-24680'
+    },
+    'xiaomi_mi_11': {
+        'name': 'Xiaomi Mi 11',
+        'category': 'Phones',
+        'price': 799,
+        'nickname': 'Mi11-13579'
+    },
+    'sony_xperia_1_iii': {
+        'name': 'Sony Xperia 1 III',
+        'category': 'Phones',
+        'price': 1199,
+        'nickname': 'Xperia1III-98765'
+    },
+    'lg_g8_thinq': {
+        'name': 'LG G8 ThinQ',
+        'category': 'Phones',
+        'price': 599,
+        'nickname': 'G8ThinQ-75319'
+    },
+    'nokia_8_3': {
+        'name': 'Nokia 8.3',
+        'category': 'Phones',
+        'price': 649,
+        'nickname': 'Nokia8.3-15926'
+    },
+    'huawei_matepad_pro': {
+        'name': 'Huawei MatePad Pro',
+        'category': 'Tablets',
+        'price': 699,
+        'nickname': 'MatePadPro-35791'
+    },
+    # Add more products as needed
 }
+
+
+# # Updated PRODUCTS with Japanese names and nicknames
+# PRODUCTS = {
+#     'Iphone': {
+#         'name': 'アイフォン 5S',
+#         'category': 'スマートフォン',
+#         'price': 699,
+#         'nickname': 'アップル5S-12345'
+#     },
+#     'galaxy': {
+#         'name': 'サムスンギャラクシー 5',
+#         'category': 'スマートフォン',
+#         'price': 649,
+#         'nickname': 'ギャラクシー5-67890'
+#     },
+#     'ipad-air': {
+#         'name': 'アイパッド エア',
+#         'category': 'タブレット',
+#         'price': 649,
+#         'nickname': 'エア-54321'
+#     },
+#     'ipad-mini': {
+#         'name': 'アイパッド ミニ',
+#         'category': 'タブレット',
+#         'price': 549,
+#         'nickname': 'ミニ-09876'
+#     }
+# }
